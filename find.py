d20fe0bd5d77753b708b900d090dfe7288708844
Adesh Kumar Pal <68577892+Adeclak@users.noreply.github.com>
2024-05-06 23:58:58 +0530

    Update find.py

--- find.py
+++ find.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 client = MongoClient('mongodb://localhost:27017')
 print(client)
-db = client['Mrigyank']
+db = client['Adesh']
 collection = db['Parking']
 
 exit_num = input("Vehicle Number = ")
